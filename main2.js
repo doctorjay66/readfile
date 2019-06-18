d0f62d1e89206943171dd25dab70b6a1cc70a244
doctorjay66 <doctor.jay66@gmail.com>
2019-06-18 10:29:14 +0200

    Update main2.js

--- main2.js
+++ main2.js
@@ -24,3 +24,54 @@ function onGetFileSystemSuccess(fs) {
   // Get a list of all the entries in the directory
   dr.readEntries(onDirReaderSuccess, onFileError);
 }
+
+function onDirReaderSuccess(dirEntries) {
+  alert("onDirReaderSuccess (" + dirEntries.length + ")");
+}
+
+function onFileError(e) {
+  var msgText;
+  switch(e.code) {
+    case FileError.NOT_FOUND_ERR:
+      msgText = "File not found error.";
+      break;
+    case FileError.SECURITY_ERR:
+      msgText = "Security error.";
+      break;
+    case FileError.ABORT_ERR:
+      msgText = "Abort error.";
+      break;
+    case FileError.NOT_READABLE_ERR:
+      msgText = "Not readable error.";
+      break;
+    case FileError.ENCODING_ERR:
+      msgText = "Encoding error.";
+      break;
+    case FileError.NO_MODIFICATION_ALLOWED_ERR:
+      msgText = "No modification allowed.";
+      break;
+    case FileError.INVALID_STATE_ERR:
+      msgText = "Invalid state.";
+      break;
+    case FileError.SYNTAX_ERR:
+      msgText = "Syntax error.";
+      break;
+    case FileError.INVALID_MODIFICATION_ERR:
+      msgText = "Invalid modification.";
+      break;
+    case FileError.QUOTA_EXCEEDED_ERR:
+      msgText = "Quote exceeded.";
+      break;
+    case FileError.TYPE_MISMATCH_ERR:
+      msgText = "Type mismatch.";
+      break;
+    case FileError.PATH_EXISTS_ERR:
+      msgText = "Path exists error.";
+      break;
+    default:
+      msgText = "Unknown error.";
+  }
+  //Now tell the user what happened
+  //navigator.notification.alert(msgText, null, "File Error");
+  alert(msgText, null, "File Error");
+}
