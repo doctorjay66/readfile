2815c1df3f7d7c910ab59cfa8a41c86f77d8da58
doctorjay66 <doctor.jay66@gmail.com>
2019-06-18 10:24:30 +0200

    Update main2.js

--- main2.js
+++ main2.js
@@ -1,6 +1,26 @@
+var theEntry;
+var theEntries;
+var theFileSystem;
+var br = '<br />';
+var hr = '<hr />';
+var startP = '<p>';
+var endP = '</p>';
+
 function processDir(fileSystemType) {
       alert("processDir: " + fileSystemType);
       //Get a handle to the local file system (allocate 1 Mb for
       // storage)
       window.requestFileSystem(fileSystemType, 1024 * 1024, onGetFileSystemSuccess, onFileError);
+}
+
+function onGetFileSystemSuccess(fs) {
+  alert("onGetFileSystemSuccess: " + fs.name + " storage (" + fs.root.fullPath + ")");
+  //Save the file system object so we can access it later
+  //Yes, I know it's cheating, but it's an easier way to do this
+  theFileSystem = fs;
+  //Create a directory reader we'll use to list the files in the
+  // directory
+  var dr = fs.root.createReader();
+  // Get a list of all the entries in the directory
+  dr.readEntries(onDirReaderSuccess, onFileError);
 }
