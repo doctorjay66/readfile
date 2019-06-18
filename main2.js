fd2ca42743d107552fdfa1936e14d456d142cf10
doctorjay66 <doctor.jay66@gmail.com>
2019-06-18 10:20:21 +0200

    Update main2.js

--- main2.js
+++ main2.js
@@ -1,8 +1,6 @@
-function onBodyLoad() {
-  document.addEventListener("deviceready", onDeviceReady, false);
-}
-
-function onDeviceReady() {
-  navigator.notification.alert("PhoneGap is ready!");
-  alert("PhoneGap is ready!");
+function processDir(fileSystemType) {
+      alert("processDir: " + fileSystemType);
+      //Get a handle to the local file system (allocate 1 Mb for
+      // storage)
+      window.requestFileSystem(fileSystemType, 1024 * 1024, onGetFileSystemSuccess, onFileError);
 }
