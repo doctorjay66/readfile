227657d198f387b5daaf5acf11af9512a0e8bae8
doctorjay66 <doctor.jay66@gmail.com>
2019-06-26 11:40:24 +0200

    Update main2.js

--- main2.js
+++ main2.js
@@ -34,7 +34,7 @@ function gotFileToSend(fileEntry) {
 function transFile() {
       alert("in trans file");
       //window.resolveLocalFileSystemURL("file:///storage/emulated/0/Download/testbla.txt", gotFile, onFileError);
-      var data_to_tras = "bla bla!";
+      var data_to_tras = "file=bla bla!";
       var oReq = new XMLHttpRequest();
       oReq.open("POST", "http://www.ausl.bologna.it/applications/test/getTransFile", true);
       oReq.onload = function (oEvent) {
